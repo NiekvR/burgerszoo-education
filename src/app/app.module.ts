2b8664a56167e98ac1619e2c8e744f486b473996
niek <niekvrabenswaaij@gmail.com>
2025-12-04 20:42:17 +0100

    Update firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppRoutingModule } from './routing.module';
 import { QuizModule } from './quiz/quiz.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SearchCardsModule } from './search-cards/search-cards.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,19 @@ import { SearchCardsModule } from './search-cards/search-cards.module';
     AppRoutingModule,
     QuizModule,
     FontAwesomeModule,
-    SearchCardsModule
+    SearchCardsModule,
+    provideFirebaseApp(() => initializeApp(
+      {
+        apiKey: "AIzaSyCVcRYJzRm0TIrlTof1Ye55tXPiteBGNgU",
+        authDomain: "burgerszoo--scavenger-hunt.firebaseapp.com",
+        projectId: "burgerszoo--scavenger-hunt",
+        storageBucket: "burgerszoo--scavenger-hunt.firebasestorage.app",
+        messagingSenderId: "500088961747",
+        appId: "1:500088961747:web:90c821f5d4cd9026767226"
+      }
+    )),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
